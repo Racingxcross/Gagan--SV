c1368972334d28179c585daa3bafb0a578e8c8b0
Gagansv <gagansv.xworkz@gmail.com>
2022-09-15 08:08:38 +0530

    mobilerunner

---
+++ NameByMobileNumberRunner.java
@@ -0,0 +1,13 @@
+class NameByMobileNumberRunner{
+	public static void main(String[] Name)
+	{
+		
+		System.out.println("Entered NameByMobileNumber");
+		
+		String ref=NameByMobileNumber.getNameByMobile(9449685848L);
+        System.out.println(ref);
+		
+	}
+	
+
+}
