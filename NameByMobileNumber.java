0ae0cda0ed5dfb693b0940beed0fbae9b53f9b84
Gagansv <gagansv.xworkz@gmail.com>
2022-09-15 08:04:09 +0530

    namebymobilenumber

---
+++ NameByMobileNumber.java
@@ -0,0 +1,58 @@
+class NameByMobileNumber{
+	static String getNameByMobile(long number)
+{
+	System.out.println("Entered getNameByMobile");
+	    
+		if (number==8277738399L) //1
+	{
+		System.out.println("Mobile Number is :8277738399");
+		return "Srisham";
+	}
+		if(number==9448722122L) //2
+	{
+		System.out.println("Mobile Number is :9448722122");
+		return "S L S giri";
+	}
+		if (number==9481064199L) //3
+	{
+		System.out.println("Mobile Number is :9481064199");
+		return "S L M giri";
+	}
+		if(number==9743445720L) //4
+	{
+		System.out.println("Mobile Number is :9743445720");
+		return "Kantu";
+	}
+		if (number==9481535704L) //5
+	{
+		System.out.println("Mobile Number is :9481535704");
+		return "Sumanth";
+	}
+		if(number==9449594341L) //6
+	{
+		System.out.println("Mobile Number is :9449594341");
+		return "B N";
+	}
+		if (number==9449685848L) //7
+	{
+		System.out.println("Mobile Number is :9449785848");
+		return "Suri";
+	}
+		if(number==8185253658L) //8
+	{
+		System.out.println("Mobile Number is :08185253658");
+		return "Home";
+	}
+		if (number==9449286046L) //9
+	{
+		System.out.println("Mobile Number is :9449286046");
+		return "Vinay";
+	}
+		if(number==9988776655L) //10
+	{
+		System.out.println("Mobile Number is :9988776655L");
+		return "Spam";
+	}
+		return null;
+} 
+}
