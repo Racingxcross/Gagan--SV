3d6ecb8c809fdb8b1143a3a83fd2bbe83e0306a9
Gagansv <gagansv.xworkz@gmail.com>
2022-09-15 07:52:42 +0530

    Array files

---
+++ Array.java
@@ -0,0 +1,63 @@
+class Array{
+	public static void main (String[]Toxic)
+	{
+		System.out.println("We Declaring the Prouduct And Price in a Array using a double type Data");
+		
+		int Perfume=299;
+		int Trimmer=1999;
+		int HairDryer=1499;
+		int HairHeater=999;
+		int Bodyloation=599;
+		int Bottle=199;
+		int Helmate=499;
+		int Himalaya=59;
+		int Santhoor=39;
+		int Dove=49;
+		int Shampoo=249;
+		int Conditioner=248;
+		int Facewash=269;
+		int Board=999;
+		int Laptop=59999;
+		int Mobile=15999;
+		int Chair=2000;
+		int Speaker=4549;
+		int Mask=99;
+        int Cooker=350;
+		int Wheel=1049;
+		int Oil=59;
+		int Bags=499;
+		int Shirt=1299;
+		int Watch=1599;
+		int Shoes=2999;
+		int HairPods=2599;
+        int KeyBoard=1499;
+		int Umbrelaa=339;
+		int Fridge=25479;
+		int PhotoFrame=2599;
+		int GoldRing=9999;
+		int Linears=599;
+		int Wallets=599;
+		int Foundation=1299;
+		int Eyelighter=259;
+		int EyeShadow=269;
+		int Knief=179;
+		int Lipstick=899;
+		int Coconutoil=357;
+		int Balls=89;
+		int Cups=125;
+		int Charger=459;
+		int WristWatch=4559;
+		int Cap=785;
+		int Cylinder=9959;
+		int HairGel =439;
+		
+		int[]listoftheproduct={Perfume,Trimmer,HairDryer,HairHeater,Bodyloation,Cylinder,Cap,WristWatch,Chair,Charger,Balls,Cups,Coconutoil,Lipstick,Knief,EyeShadow,Eyelighter,Foundation,Wallets,Linears,GoldRing,PhotoFrame,Fridge,Umbrelaa,KeyBoard,HairPods,Shoes,Watch,Shirt,Bags,Oil,Wheel,Cooker,Mask,Speaker,Mobile,Laptop,Board,Facewash,Conditioner,Shampoo,Dove,Santhoor,Himalaya,Helmate,Bottle,HairGel};	
+		int count =listoftheproduct.length;
+		System.out.println("Least the total Num of Product :- " + count);
+		for(int array=0; array<listoftheproduct.length; array++)
+		{
+			ints ref=listoftheproduct[array];
+			System.out.println(""+ref);
+		}
+}
+}
--- Forest.java
+++
@@ -1,9 +0,0 @@
-class Forest{
-		
-		static String name="shettihalliWildLifeSanctuary";
-		static double totalArea=395.60D;
-		static String type="reserveForest";
-		static String region="shimogga";
-	    static String[] primaryAnimals={"tiger","leapord","elephant","slothBear","gaintFlyingSquirrel"};
-
-	}
